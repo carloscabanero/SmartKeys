20317410650a3fd8de017fd1063df13acf38ea3c
Atul M <atul3189@gmail.com>
2016-10-26 23:21:39 +0530

    Added SKModifierButton : UIButton, to implement blink blue background for modifier keys with animation

---
+++ smartkeys/SKModifierButton.h
@@ -0,0 +1,15 @@
+//
+//  SKModifierButton.h
+//  smartkeys
+//
+//  Created by Atul M on 26/10/16.
+//  Copyright © 2016 CARLOS CABANERO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKModifierButton : UIButton
+
+@property (nonatomic, strong) CALayer *backgroundLayer;
+
+@end
